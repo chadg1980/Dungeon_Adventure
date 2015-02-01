c53fc084c662c1b0fed5e34870badff171555958
chadg1980 <chadg1980@gmail.com>
2015-01-31 22:06:59 -0800

    A days work
    Many Many changes, most work, some don't
    Massive progress today, still no further than yesterday.

--- glaserc.adventure.c
+++ glaserc.adventure.c
@@ -18,10 +18,10 @@
 struct thisRoom{
 	char roomName[20];		//just the name of the room
 	char fullName[80];		//full path and file name of each room file
-	
+	int roomNum;			//Because comparing strings is painfull, each room will get a unique number
 	int type;				//type string in file, but int for game. (1 start, 3 finish, 2 mid)
-	char connection[6][80];	//Connection 1 to 6
-	
+	char connection[10][80];	//Connection 1 to 6
+	int conCount;
 };
 
 
@@ -37,20 +37,32 @@ void swap (int *a, int *b){
 void randArr(int arr[], int n){
 	int j, k;
 	
-	srand( time(NULL)); 
+	
 	for (j = n-1; j > 0; j--){
 		k = rand() % (j+1);
 		swap(&arr[j], &arr[k]);
 	}
 }
-/*double check the numbers are random
+
+void AssignRoom(int arr[], int n){
+	int j, k;
+	
+	
+	for (j = n-1; j > 0; j--){
+		k = rand() % (j+1);
+		swap(&arr[j], &arr[k]);
+	}
+}
+
+
+/*double check the numbers are random*/
 void printArr( int arr[], int n){
 	int i;
 	for (i = 0; i < n; i++)
 		printf("num: %d, ", arr[i]);
 	printf("\n");
 }
-*/
+
 int permissions(){
 	/*I got a hint about file permission being octal
 	http://bytes.com/topic/c/answers/459585-string-octal */
@@ -64,11 +76,12 @@ int permissions(){
 
 /*-------------------MAIN STARTS HERE-------------------------*/
 int main(){
+	srand( time(NULL)); 
 	int i;
 	char myname[33] = "glaserc.rooms.";
 	char dirname[66];
 	int n = 10;
-	int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9,};
+	int arr[] = {10, 1, 2, 3, 4, 5, 6, 7, 8, 9,};
 	randArr(arr, n);
 	int start = 0;
 	int mid = 0;
@@ -76,8 +89,8 @@ int main(){
 	/*start the struct of 10 rooms*/
 	struct thisRoom room[10];
 		for(i = 0; i <10; i++){
-	struct thisRoom room[i];
-	}
+			struct thisRoom room[i];
+		}
 	
 	
 	/* to check, must uncomment the function also
@@ -94,7 +107,7 @@ int main(){
 	
 	/*---------------append pid to dirname--------------------*/
 	/*strcat(dirname, buffer);*/
-	sprintf(dirname, "%s%s",myname, buffer);
+	snprintf(dirname, sizeof(myname)+sizeof(buffer)+1,  "%s%s",myname, buffer);
 	mkdir(dirname, perm);
 	
 	
@@ -106,32 +119,125 @@ int main(){
 	
 	/*room names, the array numbers are random each run of the program*/
 	/*Names based on Seahawks schedule*/
-	sprintf(room[arr[0]].roomName, "Green Bay");
-	sprintf(room[arr[1]].roomName, "San Diego");
-	sprintf(room[arr[2]].roomName, "Denver");
-	sprintf(room[arr[3]].roomName, "Washington");
-	sprintf(room[arr[4]].roomName, "Dallas");
-	sprintf(room[arr[5]].roomName, "Carolina");
-	sprintf(room[arr[6]].roomName, "Arizona");
-	sprintf(room[arr[7]].roomName, "St Louis");
-	sprintf(room[arr[8]].roomName, "Oakland");
-	sprintf(room[arr[9]].roomName, "New York");
+	snprintf(room[arr[0]].roomName, 10, "Green Bay");
+	snprintf(room[arr[1]].roomName, 10, "San Diego");
+	snprintf(room[arr[2]].roomName, 7, "Denver");
+	snprintf(room[arr[3]].roomName, 11, "Washington");
+	snprintf(room[arr[4]].roomName, 7, "Dallas");
+	snprintf(room[arr[5]].roomName, 9, "Carolina");
+	snprintf(room[arr[6]].roomName, 8, "Arizona");
+	snprintf(room[arr[7]].roomName, 9, "St Louis");
+	snprintf(room[arr[8]].roomName, 8, "Oakland");
+	snprintf(room[arr[9]].roomName, 9, "New York");
 	
 	/*print out the rooms, so I know the order, comment out for final product*/
-	for (i = 0; i < 10; i++){
-	
-	printf("Thisroom: %s\n", room[i].roomName);
+	for (i = 1; i <= 10; i++){
+		room[i].roomNum = i;
+		room[i].conCount = 0;
+		printf("This roomname: %s\n", room[i].roomName);
 	}
 	
-	/*make the files*/
-	for( i = 0; i < 7; i++){
-		/*file is recreated each iteration*/
-		char localFile[80];
-		FILE *fpoint;
-		int k, q;
+	for(i = 1; i <=7; i++){
 		
-		sprintf(room[i].fullName, "./%s/%s", dirname, room[i].roomName);
-			//strcpy(localFile, room[i].fullName);
+		int AssignRoom[] = {1, 2, 3, 4, 5, 6, 7};
+		int c= 7;
+		randArr(AssignRoom, c);
+		printArr(AssignRoom, c);
+		int kk;
+		int num;
+		int k, l, q, connections, ret, REVconnections, Rret;
+		
+		/* q is the number of connections 3 to 6*/	
+		q = rand() %4 + 3;
+		
+		/*inner loop*/
+		for(k = 0; k <= q; k++){
+			
+			ret = 99;
+			num = AssignRoom[k];
+			
+			/*check to see if there is already a connection*/
+			connections = room[i].conCount;
+			for(l = 1; l < connections; l++){
+				ret = strcmp(room[i].connection[l], room[num].roomName);
+				if(ret == 0){
+					printf("already connected\n");
+					break;
+				}		
+			}
+			
+			/*check the connection count, if the room already has 6 connections, skip*/
+			if(room[i].conCount >=6){
+				printf("connections maxed\n");
+				k = q;
+			}
+			else if(ret == 0)
+				printf("do nothing\n");
+			else {
+				/*Check if the room is going to assign itself as a connection*/
+				if(room[num].roomNum == room[i].roomNum){
+				 printf("can't connect to yourself\n");
+					if(num <= 3){
+						num= AssignRoom[k+ 1];
+					}
+					else{
+						num= AssignRoom[k -1];
+					}
+				}
+				
+				
+				/*add the connection to the list of connections*/
+				room[i].conCount++;
+				snprintf(room[i].connection[room[i].conCount], sizeof(room[num].roomName),
+										room[num].roomName); 
+				printf("%s, connects with ----->",room[i].roomName);
+				printf("CONNECTION %d: %s\n", room[i].conCount, room[num].roomName);
+				
+				
+				/*reverse link*/
+				kk = room[num].conCount;
+			
+				/*check to see if there is already a connection*/
+				REVconnections = room[num].conCount;
+				for(l = 1; l < REVconnections; l++){
+					ret = strcmp(room[num].connection[l], room[i].roomName);
+					if(Rret == 0){
+						printf("RETURN..already connected\n");
+						break;
+					}		
+				}
+				
+				if ((kk >= 6) || Rret == 0){
+					printf("connections maxed out\n");
+				}
+				else{
+				
+				kk++;
+				snprintf(room[num].connection[kk], sizeof(room[i].roomName), room[i].roomName); 
+				printf("REVERSE: %s  ------>", room[num].roomName);
+				printf("CONNECTION %d: %s\n", room[num].conCount, room[i].roomName);
+				
+				room[num].conCount++;
+				}
+			}
+			
+			
+			
+		
+		}
+	
+	}
+	/*make the files*/
+	for( i = 1; i <= 7; i++){
+		int w = 0;
+		int j, cons;
+		/*file is recreated each iteration*/
+		FILE *fpoint;
+		
+		
+		snprintf(room[i].fullName, sizeof(dirname) 
+									+ sizeof(room[i].roomName) +1, "./%s/%s", dirname, room[i].roomName);
+			
 		
 		fpoint = fopen(room[i].fullName, "a");
 		if (fpoint == 0){
@@ -142,30 +248,16 @@ int main(){
 		
 		/*write the room name to a file*/
 		fprintf(fpoint, "ROOM NAME: %s\n", room[i].roomName); 
+		printf("ROOM NAME: %s\n", room[i].roomName); 
 		
 		/*Assign Connections*/
-			/*room i, connection k*/
-		q = rand() %4 + 3;
-		for(k = 1; k <= q; k++){	
-			if( (i + k) < 6){
-					sprintf(room[i].connection[k], room[i+k].roomName); 
-					fprintf(fpoint, "CONNECTION %d: %s\n", k, room[i+k].roomName);
-					printf("CONNECTION %d: %s\n", k, room[i+k].roomName);
-				}
-			else if ((i - k) >= 0){
-					sprintf(room[i].connection[k], room[i-k].roomName); 
-					fprintf(fpoint, "CONNECTION %d: %s\n", k, room[i-k].roomName);
-					printf("CONNECTION %d: %s\n", k, room[i-k].roomName);
-			}
-			else{
-					sprintf(room[i].connection[k], room[k-i].roomName); 
-					fprintf(fpoint, "CONNECTION %d: %s\n", k, room[k-i].roomName);
-					printf("CONNECTION %d: %s\n", k, room[k-i].roomName);
-			}
+		cons = room[i].conCount;
+		for(j = 1; j <= cons; j++)
+			fprintf(fpoint, "CONNECTION %d: %s\n", j, room[i].connection[j]);
 		
-		}
+		
 		/*assigning room type(pseudo random)*/
-			int w = 0;
+			
 			w = rand() % 3 + 1;
 			if( w == 1){
 				if(start == 0){
@@ -210,6 +302,7 @@ int main(){
 			
 		
 		fclose(fpoint);
+		printf("file: %s, closed \n", room[i].fullName);
 	}
 	
 	
